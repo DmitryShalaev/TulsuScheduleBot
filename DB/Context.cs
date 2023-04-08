b303fa432a03ef04ca7ee29eadf4ab9675c8bf59
Dmitry <62142602+DmitryShalaev@users.noreply.github.com>
2023-04-08 20:37:35 +0300

    Добавьте файлы проекта.

---
+++ DB/Context.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using ScheduleBot.DB.Entity;
+
+namespace ScheduleBot.DB {
+    public class ScheduleDbContext : DbContext {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlite($"Data Source={Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "MyListDB.db")}");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+
+        }
+
+        public void CleanDB() {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+            SaveChanges();
+        }
+
+#pragma warning disable CS8618
+        public DbSet<Disciplines> Disciplines { get; set; }
+
+    }
+}
