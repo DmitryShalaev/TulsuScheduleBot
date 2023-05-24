cd8362aceb6e39ef9fa4c4caf7bd7eb98871800e
Dmitry <62142602+DmitryShalaev@users.noreply.github.com>
2023-05-24 20:18:27 +0300

    upd: the functions are placed in a separate file

---
+++ Bot/ChangeProfileMessageMode.cs
@@ -0,0 +1,76 @@
+﻿using ScheduleBot.DB.Entity;
+
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace ScheduleBot.Bot {
+    public partial class TelegramBot {
+        private async Task GroupСhangeMessageMode(ITelegramBotClient botClient, Message message, TelegramUser user) {
+            switch(message.Text) {
+                case Constants.RK_Cancel:
+                    user.Mode = Mode.Default;
+                    dbContext.SaveChanges();
+
+                    await botClient.SendTextMessageAsync(chatId: message.Chat, text: "Основное меню", replyMarkup: MainKeyboardMarkup);
+                    break;
+
+                default:
+                    await botClient.SendTextMessageAsync(chatId: message.Chat, text: "Нужно подождать...", replyMarkup: CancelKeyboardMarkup);
+                    Parser parser = new(dbContext, false);
+                    if(parser.GetDates(message.Text ?? "") is not null) {
+                        bool flag = dbContext.ScheduleProfile.Select(i=>i.Group).Contains(message.Text);
+
+                        user.Mode = Mode.Default;
+                        user.ScheduleProfile.Group = message.Text;
+                        dbContext.SaveChanges();
+
+                        await botClient.SendTextMessageAsync(chatId: message.Chat, text: $"Номер группы успешно изменен на {message.Text} ", replyMarkup: MainKeyboardMarkup);
+
+                        if(!flag)
+                            parser.UpdatingDisciplines(message.Text);
+                    } else {
+                        await botClient.SendTextMessageAsync(chatId: message.Chat, text: "Сайт ТулГУ не отвечает или такой группы не существует", replyMarkup: CancelKeyboardMarkup);
+                    }
+
+                    break;
+            }
+
+        }
+
+        private async Task StudentIDСhangeMessageMode(ITelegramBotClient botClient, Message message, TelegramUser user) {
+            switch(message.Text) {
+                case Constants.RK_Cancel:
+                    user.Mode = Mode.Default;
+                    dbContext.SaveChanges();
+
+                    await botClient.SendTextMessageAsync(chatId: message.Chat, text: "Основное меню", replyMarkup: MainKeyboardMarkup);
+                    break;
+
+                default:
+                    await botClient.SendTextMessageAsync(chatId: message.Chat, text: "Нужно подождать...", replyMarkup: CancelKeyboardMarkup);
+
+                    if(int.TryParse(message.Text ?? "", out int studentID)) {
+                        Parser parser = new(dbContext, false);
+                        if(parser.GetProgress(message.Text ?? "") is not null) {
+                            bool flag = dbContext.ScheduleProfile.Select(i=>i.StudentID).Contains(message.Text);
+
+                            user.Mode = Mode.Default;
+                            user.ScheduleProfile.StudentID = studentID.ToString();
+                            dbContext.SaveChanges();
+
+                            await botClient.SendTextMessageAsync(chatId: message.Chat, text: $"Номер зачётки успешно изменен на {message.Text} ", replyMarkup: MainKeyboardMarkup);
+
+                            if(!flag)
+                                parser.UpdatingProgress(studentID.ToString());
+                        } else {
+                            await botClient.SendTextMessageAsync(chatId: message.Chat, text: "Сайт ТулГУ не отвечает или указан неверный номер зачётки", replyMarkup: CancelKeyboardMarkup);
+                        }
+
+                    } else {
+                        await botClient.SendTextMessageAsync(chatId: message.Chat, text: "Не удалось распознать введенный номер зачётной книжки", replyMarkup: CancelKeyboardMarkup);
+                    }
+                    break;
+            }
+        }
+    }
+}
