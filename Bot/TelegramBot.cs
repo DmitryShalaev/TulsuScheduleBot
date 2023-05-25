55556e35d17fa8e2b7078c9c0caa35bfbd99fdf0
Dmitry <62142602+DmitryShalaev@users.noreply.github.com>
2023-05-25 17:27:36 +0300

    upd: InlineQuery cacheTime

--- Bot/TelegramBot.cs
+++ Bot/TelegramBot.cs
@@ -126,7 +126,7 @@ namespace ScheduleBot.Bot {
                             await botClient.AnswerInlineQueryAsync(inlineQuery.Id, new[] {
                                 new InlineQueryResultArticle(Constants.RK_Today, Constants.RK_Today, new InputTextMessageContent(scheduler.GetScheduleByDate(DateOnly.FromDateTime(DateTime.Now), user.ScheduleProfile))),
                                 new InlineQueryResultArticle(Constants.RK_Tomorrow, Constants.RK_Tomorrow, new InputTextMessageContent(scheduler.GetScheduleByDate(DateOnly.FromDateTime(DateTime.Now.AddDays(1)), user.ScheduleProfile)))
-                            });
+                            }, cacheTime: 60 * 10, isPersonal: true);
                         }
                     }
                     break;
