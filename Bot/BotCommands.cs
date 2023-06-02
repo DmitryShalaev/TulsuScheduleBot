1aaa93bfa629ad890c6e2aba72a9ef8211cdbc9f
Dmitry <62142602+DmitryShalaev@users.noreply.github.com>
2023-06-02 21:22:59 +0300

    deleted gist

--- Bot/BotCommands.cs
+++ Bot/BotCommands.cs
@@ -26,35 +26,19 @@ namespace ScheduleBot.Bot {
         public CollegeStruct College { get; private set; }
 
         public BotCommands() {
-            JObject commands;
+            using(StreamReader sr = new("Bot/TulsuScheduleBotSettings.json")) {
+                JObject commands = JObject.Parse(sr.ReadToEnd());
 
-            try {
-                using(var client = new HttpClient()) {
-                    using(HttpResponseMessage response = client.GetAsync(Environment.GetEnvironmentVariable("TelegramBotSettings")).Result) {
-                        if(response.IsSuccessStatusCode) {
-#if DEBUG
-                            throw new Exception();
-#else
-                            commands = JObject.Parse(response.Content.ReadAsStringAsync().Result);
-#endif
-                        } else {
-                            throw new Exception();
-                        }
-                    }
-                }
-            } catch(Exception) {
-                using(StreamReader sr = new("Bot/TulsuScheduleBotSettings.json"))
-                    commands = JObject.Parse(sr.ReadToEnd());
+
+                Message = commands["Message"]?.ToObject<Dictionary<string, string>>() ?? throw new NullReferenceException("Message");
+                Callback = commands["Callback"]?.ToObject<Dictionary<string, CallbackStruct>>() ?? throw new NullReferenceException("Callback");
+                Corps = commands["Corps"]?.ToObject<CorpsStruct[]>() ?? throw new NullReferenceException("Corps");
+                StagesOfAdding = commands["StagesOfAdding"]?.ToObject<string[]>() ?? throw new NullReferenceException("StagesOfAdding");
+                College = commands["College"]?.ToObject<CollegeStruct>() ?? throw new NullReferenceException("College");
+
+                Message.TrimExcess();
+                Callback.TrimExcess();
             }
-
-            Message = commands["Message"]?.ToObject<Dictionary<string, string>>() ?? throw new NullReferenceException("Message");
-            Callback = commands["Callback"]?.ToObject<Dictionary<string, CallbackStruct>>() ?? throw new NullReferenceException("Callback");
-            Corps = commands["Corps"]?.ToObject<CorpsStruct[]>() ?? throw new NullReferenceException("Corps");
-            StagesOfAdding = commands["StagesOfAdding"]?.ToObject<string[]>() ?? throw new NullReferenceException("StagesOfAdding");
-            College = commands["College"]?.ToObject<CollegeStruct>() ?? throw new NullReferenceException("College");
-
-            Message.TrimExcess();
-            Callback.TrimExcess();
         }
     }
 }
