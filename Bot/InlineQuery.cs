a4067e8f4948b2760d81a705cfd198581baa20b7
Dmitry <62142602+DmitryShalaev@users.noreply.github.com>
2023-06-03 13:11:10 +0300

    fix: InlineQuery

--- Bot/InlineQuery.cs
+++ Bot/InlineQuery.cs
@@ -36,9 +36,12 @@ namespace ScheduleBot.Bot {
         }
 
         private async Task AnswerInlineQueryAsync(ITelegramBotClient botClient, InlineQuery inlineQuery, DateOnly date) {
-            TelegramUser? user = dbContext.TelegramUsers.Include(u => u.ScheduleProfile).FirstOrDefault(u => u.ChatID == inlineQuery.From.Id);
+            TelegramUser? user = dbContext.TelegramUsers.Include(u => u.ScheduleProfile).SingleOrDefault(u => u.ChatID == inlineQuery.From.Id);
 
             if(user is not null && !string.IsNullOrWhiteSpace(user.ScheduleProfile.Group)) {
+                if((DateTime.UtcNow - dbContext.GroupLastUpdate.Single(i => i.Group == user.ScheduleProfile.Group).Update).TotalMinutes > commands.Config.GroupUpdateTime)
+                    parser.UpdatingDisciplines(user.ScheduleProfile.Group);
+
                 await botClient.AnswerInlineQueryAsync(inlineQuery.Id, new[] {
                     new InlineQueryResultArticle(date.ToString(), date.ToString(), new InputTextMessageContent(scheduler.GetScheduleByDate(date, user.ScheduleProfile))),
                 }, cacheTime: 60, isPersonal: true);
