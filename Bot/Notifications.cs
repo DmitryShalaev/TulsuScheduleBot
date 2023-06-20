a245e0a142ea3ede187040337ba4d60d03daaa30
Dmitry <62142602+DmitryShalaev@users.noreply.github.com>
2023-06-20 23:18:37 +0300

    added message before submitting schedule changes

--- Bot/Notifications.cs
+++ Bot/Notifications.cs
@@ -2,14 +2,21 @@
 
 using Microsoft.EntityFrameworkCore;
 
+using ScheduleBot.DB.Entity;
+
 using Telegram.Bot;
 using Telegram.Bot.Types.ReplyMarkups;
 
 namespace ScheduleBot.Bot {
+    public class ExtendedTelegramUser : TelegramUser {
+        public ExtendedTelegramUser(TelegramUser telegramUser) : base(telegramUser) { }
+        public bool Flag { get; set; } = false;
+    }
+
     public partial class TelegramBot {
 
         private async Task UpdatedDisciplinesAsync(List<(string Group, DateOnly Date)> values) {
-            var telegramUsers = dbContext.TelegramUsers.Include(u => u.Notifications).Where(i => i.Notifications != null).Include(u => u.ScheduleProfile).ToList();
+            var telegramUsers = dbContext.TelegramUsers.Include(u => u.Notifications).Where(u => u.Notifications != null).Include(u => u.ScheduleProfile).Select(u => new ExtendedTelegramUser(u)).ToList();
 
             foreach(var item in values) {
                 int weekNumber = CultureInfo.CurrentCulture.Calendar.GetWeekOfYear(DateTime.Parse(item.Date.ToString()), CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
@@ -19,6 +26,11 @@ namespace ScheduleBot.Bot {
                 var days = (DateTime.Parse(item.Date.ToString()) - date).TotalDays;
 
                 foreach(var user in telegramUsers.Where(i => i.ScheduleProfile.Group == item.Group && days <= i.Notifications?.Days)) {
+                    if(!user.Flag) {
+                        await botClient.SendTextMessageAsync(chatId: user.ChatID, text: commands.Message["NotificationMessage"], disableNotification: IsDND(user.Notifications?.DNDStart, user.Notifications?.DNDStop));
+                        user.Flag = true;
+                    }
+
                     await botClient.SendTextMessageAsync(chatId: user.ChatID, text: str,
                             replyMarkup: new InlineKeyboardMarkup(InlineKeyboardButton.WithCallbackData(text: commands.Callback["All"].text, callbackData: $"{commands.Callback["All"].callback} {item.Date}")),
                             disableNotification: IsDND(user.Notifications?.DNDStart, user.Notifications?.DNDStop));
