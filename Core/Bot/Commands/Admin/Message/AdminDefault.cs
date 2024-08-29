5cc763261fbddf6989fddeacb7a36eb720f07fa1
Dmitry <dmitryshalaev1901@gmail.com>
2024-08-29 14:04:07 +0300

    Create AdminDefault.cs

---
+++ Core/Bot/Commands/Admin/Message/AdminDefault.cs
@@ -0,0 +1,22 @@
+﻿using Core.Bot.Commands.Interfaces;
+using Core.DB;
+using Core.DB.Entity;
+
+using Telegram.Bot.Types;
+
+namespace Core.Bot.Commands.Admin.Message {
+    internal class AdminDefault : IMessageCommand {
+
+        public List<string>? Commands => null;
+
+        public List<Mode> Modes => [Mode.Admin];
+
+        public Manager.Check Check => Manager.Check.admin;
+
+        public Task Execute(ScheduleDbContext dbContext, ChatId chatId, int messageId, TelegramUser user, string args) {
+            MessagesQueue.Message.SendTextMessage(chatId: chatId, text: UserCommands.Instance.Message["AdminPanel"], replyMarkup: Statics.AdminPanelKeyboardMarkup);
+
+            return Task.CompletedTask;
+        }
+    }
+}
