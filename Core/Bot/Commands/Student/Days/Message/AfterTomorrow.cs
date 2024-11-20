8ad9e10a2a67182c0a12cbbe25bd3fb11b90244c
Dmitry <dmitryshalaev1901@gmail.com>
2024-11-20 17:27:58 +0300

    Добавлена команда: Послезавтра

---
+++ Core/Bot/Commands/Student/Days/Message/AfterTomorrow.cs
@@ -0,0 +1,26 @@
+﻿using Core.Bot.Commands.Interfaces;
+using Core.DB;
+using Core.DB.Entity;
+
+using ScheduleBot;
+
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace Core.Bot.Commands.Student.Days.Message {
+    public class AfterTomorrow : IMessageCommand {
+        public List<string>? Commands => [UserCommands.Instance.Message["AfterTomorrow"]];
+
+        public List<Mode> Modes => [Mode.Default];
+
+        public Manager.Check Check => Manager.Check.group;
+
+        public async Task Execute(ScheduleDbContext dbContext, ChatId chatId, int messageId, TelegramUser user, string args) {
+            await Statics.ScheduleRelevanceAsync(dbContext, chatId, user.ScheduleProfile.Group!, DefaultMessage.GetMainKeyboardMarkup(user));
+            var date = DateOnly.FromDateTime(DateTime.Now.AddDays(2));
+
+            (string, bool) schedule = Scheduler.GetScheduleByDate(dbContext, date, user);
+            MessagesQueue.Message.SendTextMessage(chatId: chatId, text: schedule.Item1, replyMarkup: DefaultCallback.GetInlineKeyboardButton(date, user, schedule.Item2), parseMode: ParseMode.Markdown);
+        }
+    }
+}
